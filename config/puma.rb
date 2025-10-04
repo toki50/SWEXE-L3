b6fed8a0159df2a26cedef6af949b3021a8a657f
Tomoya Fukuda <2311140073n@ed.fuk.kindai.ac.jp>
2025-10-04 07:58:40 +0000

    Update Puma workers setting for Render deployment

--- config/puma.rb
+++ config/puma.rb
@@ -39,3 +39,7 @@ plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+
+workers Integer(ENV.fetch("WEB_CONCURRENCY") { 2 })
+threads_count = Integer(ENV.fetch("RAILS_MAX_THREADS") { 5 })
+threads threads_count, threads_count
